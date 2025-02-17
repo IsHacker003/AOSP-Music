a507b2c7a8839dcf20fea657bbcedec609bb6e19
IsHacker <ishayu301@gmail.com>
2025-02-17 14:30:10 +0530

    Trying to convert to Material Design 2

--- app/src/main/java/net/rymate/music/MediaPlaybackService.java
+++ app/src/main/java/net/rymate/music/MediaPlaybackService.java
@@ -350,7 +350,7 @@ public class MediaPlaybackService extends Service {
             mRemoteControlClient.setTransportControlFlags(flags);
         }
 
-        mPreferences = getSharedPreferences("Music", MODE_WORLD_READABLE | MODE_WORLD_WRITEABLE);
+        mPreferences = getSharedPreferences("Music", MODE_PRIVATE);
         mCardId = MusicUtils.getCardId(this);
 
         registerExternalStorageListener();
