495d2ac70f90902d81cfb6999405086389a0b9ca
IsHacker <ishayu301@gmail.com>
2025-08-04 16:30:43 +0530

    Change package name

---
+++ app/src/main/java/com/ishacker/android/music/activities/NowPlayingHandler.java
@@ -0,0 +1,22 @@
+package com.ishacker.android.music.activities;
+
+import android.os.Handler;
+import android.os.Message;
+
+/**
+ * Created by Ryan on 24/03/2018.
+ */
+
+class NowPlayingHandler extends Handler {
+
+    private final MediaPlaybackActivity activity;
+
+    NowPlayingHandler(MediaPlaybackActivity mediaPlaybackActivity) {
+        this.activity = mediaPlaybackActivity;
+    }
+
+    @Override
+    public void handleMessage(Message msg) {
+        activity.handleMessage(msg);
+    }
+}
